64b8394a1cd357f5c1ae4a30fd47b27b8511321a
Anuragstackapp <113967138+Anuragstackapp@users.noreply.github.com>
2022-11-12 14:09:43 +0530

    dsds

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(home: Scaffold(body: Center(child: Text("Ok"),)),));
+  runApp(MaterialApp(home: Scaffold(body: Center(child: Text("Yes"),)),));
 }
